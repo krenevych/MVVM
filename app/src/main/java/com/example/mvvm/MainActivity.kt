8f4ee63f46f384786dbb90adc9426d23ae0280c4
Andrii Krenevych <krenevych@knu.ua>
2026-09-24 19:03:21 +0300

    initial commit

---
+++ app/src/main/java/com/example/mvvm/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.mvvm
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.example.mvvm.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        binding.buttonStart.setOnClickListener {
+            // TODO: create code to start timer
+            Log.d(TAG, "onCreate: Timer starts")
+        }
+    }
+
+    companion object {
+        val TAG = "XXXX"
+    }
+}
